a3bc03b248c5b51cfc0bec77892838d2205929b2
Jose Manuel Romero <josemanuelromerop@gmail.com>
2018-12-13 14:54:56 -0400

    Create euler.m

---
+++ euler.m
@@ -0,0 +1,27 @@
+# Metodo de euler
+# F'(x,y) = sin(x) - ln(y)
+# P1(0.13, 0.32)
+# F(0.14) = ? => x(a) = 0.14
+# n = 4
+# n = 15
+
+printf('Metodo de euler\n');
+n = input('n: ');
+x(1) = input('x(1): ');
+x(n+1) = input('x(a): ');
+y(1) = input('y(1): ');
+
+h = abs(x(n+1) - x(1)) / n;
+
+for i = [2:n]
+  x(i) = x(1) + (i+1)*h;
+end
+
+for i = [1:n]
+  y(i+1) = y(i) + h*(sin(x(i)) - log(y(i)));
+end
+
+printf('F(xa): %.5f\n', y(i+1))
+
+# .' traspuesta de una matriz
+display([x.', y.'])
